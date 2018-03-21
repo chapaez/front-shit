1e9166d2423a7229f740db5c77fa11ead0c10d91
aberkov <aberkov@friday.ru>
2018-03-21 13:55:10 +0300

    product desktop

--- build.js
+++ build.js
@@ -1,6 +1,10 @@
 import './js/scripts.js';
 import 'slick-carousel';
 
+require("jquery");
+
+require("lightbox2");
+
 var __svg__ = { path: './img/*.svg', name: './sprite.svg' };
 __svg__ = {filename: './assets/sprite.svg'};
 require('webpack-svgstore-plugin/src/helpers/svgxhr')(__svg__);
--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,50 @@ $(function () {
         fade: true,
         cssEase: 'linear'
     });
+    $('.js-show-tablet-menu').click(function () {
+        $(this).parent().toggleClass('_close');
+    });
+    var $thumItems = $('.js-thumb-item');
+    if($thumItems.length>4){
+        $('.js-gallery-arrow').show();
+    }
+    console.log($thumItems.length);
+    var $galleryThumbnailSlick = $('.js-product-thumbnails').slick({
+        arrows:false,
+        vertical:true,
+        dots: false,
+        infinite: false,
+        speed: 500,
+        cssEase: 'linear',
+        slidesToShow:4,
+        slidesToScroll:1
+    });
+    var $galleryDetailSlick = $('.js-product-detail').slick({
+        arrows:false,
+        dots: false,
+        speed: 1,
+        cssEase: 'linear',
+        slidesToShow:1,
+        slidesToScroll:1,
+        useCSS: false,
+        infinite:true
+    });
+
+    $('.js-gallery-up').click(function() {
+        $galleryThumbnailSlick.slick("slickPrev");
+    });
+    $('.js-gallery-down').click(function() {
+        $galleryThumbnailSlick.slick("slickNext");
+    });
+
+    $('.js-gallery-detail-left').click(function() {
+        $galleryDetailSlick.slick("slickPrev");
+    });
+    $('.js-gallery-detail-right').click(function() {
+        $galleryDetailSlick.slick("slickNext");
+    });
+    $thumItems.click(function () {
+        $galleryDetailSlick.slick('slickGoTo', $(this).data('num'));
+    });
 
 });
